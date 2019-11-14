4b0b21a6ac0ac9670400c6c8a27c0f7ab93afb45
Dominik Andrae <dominik.andrae@inside-m2m.de>
2019-11-14 14:30:33 +0100

    chore(Webapp): generate new API

--- main/Webapp/src/api/api.module.ts
+++ main/Webapp/src/api/api.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { ApiConfiguration, ApiConfigurationInterface } from './api-configuration';
 
 import { AchievementControllerService } from './services/achievement-controller.service';
-import { BasicErrorControllerService } from './services/basic-error-controller.service';
 
 /**
  * Provider for all Api services, plus ApiConfiguration
@@ -19,8 +18,7 @@ import { BasicErrorControllerService } from './services/basic-error-controller.s
   declarations: [],
   providers: [
     ApiConfiguration,
-    AchievementControllerService,
-    BasicErrorControllerService
+    AchievementControllerService
   ],
 })
 export class ApiModule {
