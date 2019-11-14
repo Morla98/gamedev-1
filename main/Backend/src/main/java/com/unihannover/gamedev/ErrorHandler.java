d0c4009295e868897e2ae0363dce75c217913294
Dominik Andrae <dominik.andrae@inside-m2m.de>
2019-11-14 14:30:16 +0100

    feat(Backend): add config for errors and index.html redirects, edit swagger config to ignore error handler, add BaseController to force backend root url

---
+++ main/Backend/src/main/java/com/unihannover/gamedev/ErrorHandler.java
@@ -0,0 +1,28 @@
+package com.unihannover.gamedev;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorHandler implements ErrorController {
+
+	private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "forward:/index.html";
+    }
+    
+    @RequestMapping(value = "/**/{path:[^.]*}")       
+    public String redirect() {
+        // Forward to home page so that route is preserved.
+        return "forward:/index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+}
---
+++ main/Backend/src/main/java/com/unihannover/gamedev/restcontroller/BaseController.java
@@ -0,0 +1,8 @@
+package com.unihannover.gamedev.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public abstract class BaseController {}
