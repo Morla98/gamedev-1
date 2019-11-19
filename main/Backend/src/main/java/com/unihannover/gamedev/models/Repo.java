d4755cef948c26ae02b4933c0232bdb9513e161b
amrit <amritgaire18@gmail.com>
2019-11-19 13:23:02 +0100

    models like Achievements, user and Repo basics implemented

---
+++ main/Backend/src/main/java/com/unihannover/gamedev/models/Repo.java
@@ -0,0 +1,22 @@
+package com.unihannover.gamedev.models;
+
+/**
+ * Represents the repos that user has access to.
+ * @author amrit
+ * @version 0.0.1
+ */
+public class Repo {
+	private String repoName;
+	private Achievement achievement;
+	public String getRepoName() {
+		return repoName;
+	}
+	
+	
+	public void setRepoName(String repoName) {
+		this.repoName = repoName;
+	}
+	
+	
+}
+
