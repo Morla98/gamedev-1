a150898cf0b880987bd63055ff74293ef238fd02
Lukas Allermann <Lukas.Allermann@live.de>
2019-11-20 23:34:57 +0100

    implemented Collector/CollectorController

--- main/Collector/DummyCollector/main/Backend/src/main/java/com/unihannover/gamedev/models/Collector.java
+++ main/Collector/DummyCollector/main/Backend/src/main/java/com/unihannover/gamedev/models/Collector.java
@@ -1,4 +1,32 @@
 package com.unihannover.gamedev.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
 public class Collector {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private List<Achievement> achievements;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- main/Collector/DummyCollector/main/Backend/src/main/java/com/unihannover/gamedev/restcontroller/CollectorController.java
+++ main/Collector/DummyCollector/main/Backend/src/main/java/com/unihannover/gamedev/restcontroller/CollectorController.java
@@ -1,4 +1,26 @@
 package com.unihannover.gamedev.restcontroller;
 
+import com.unihannover.gamedev.models.Achievement;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.spring.web.json.Json;
+
+@RestController
 public class CollectorController {
+
+
+    /**
+     * Initialize the Collector on the main application
+     */
+    public void init(){
+    }
+
+    /**
+     * gets called on hook from application
+     */
+    @CrossOrigin(origins = "http://localhost:9082")
+    @RequestMapping(value="/update", method = RequestMethod.POST)
+    public void update(@RequestBody Json data){
+    }
+
+
 }
