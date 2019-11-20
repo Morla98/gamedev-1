dd9b44a6dc6724bdf3561a85bb9bb2a9e3e363ca
Lukas Allermann <Lukas.Allermann@live.de>
2019-11-21 00:09:02 +0100

    Revert "renamed UniHannoverApplication"
    This reverts commit d47e0294f5f6f829d327b0e2b25ea95ab1a6601d.

--- main/Collector/DummyCollector/main/Backend/src/main/java/com/unihannover/gamedev/UniHannoverApplication.java
+++ main/Collector/DummyCollector/main/Backend/src/main/java/com/unihannover/gamedev/UniHannoverApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CollectorApplication {
+public class UniHannoverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CollectorApplication.class, args);
+		SpringApplication.run(UniHannoverApplication.class, args);
 	}
 
 }
