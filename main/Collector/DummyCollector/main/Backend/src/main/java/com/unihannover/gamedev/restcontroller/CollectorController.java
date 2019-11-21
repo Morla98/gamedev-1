ae1b8eeebbf97c6e8d1b15247c3d35cafcd474c1
Lukas Allermann <Lukas.Allermann@live.de>
2019-11-21 14:07:32 +0100

    added sendPostRequest Method

--- main/Collector/DummyCollector/main/Backend/src/main/java/com/unihannover/gamedev/restcontroller/CollectorController.java
+++ main/Collector/DummyCollector/main/Backend/src/main/java/com/unihannover/gamedev/restcontroller/CollectorController.java
@@ -4,14 +4,50 @@ import com.unihannover.gamedev.models.Achievement;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.spring.web.json.Json;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @RestController
 public class CollectorController {
-
+    private static String requestUrl = "localhost:8082";
 
     /**
      * Initialize the Collector on the main application
      */
     public void init(){
+    }
+
+    private static String sendPostRequest(String payload) {
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+            writer.write(payload);
+            writer.close();
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer jsonString = new StringBuffer();
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonString.append(line);
+            }
+            br.close();
+            connection.disconnect();
+            return jsonString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
     }
 
     /**
