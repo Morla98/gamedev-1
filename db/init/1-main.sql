d268c4df877c7093346282323ae17ec177ec8550
leoncurth <leon.curth@gmx.de>
2019-11-22 22:31:03 +0100

    feat(DB) implemented Version2 of DB

--- db/init/1-main.sql
+++ db/init/1-main.sql
@@ -5,27 +5,44 @@ CREATE SCHEMA "main";
 
 -- TODO: Benutzer "main" erstellen und zum Owner des Schemas machen?
 
--- TODO: Tabelle users verbessern
-CREATE TABLE main."users" (
-  "email" TEXT PRIMARY KEY,
-  "name" TEXT NOT NULL,
-  "anon" BOOLEAN NOT NULL,
-  "level" NUMERIC NOT NULL,
-  "score" NUMERIC NOT NULL
+-- Table managing the users
+CREATE TABLE main.users (
+	email TEXT NOT NULL unique PRIMARY KEY,
+	UID SERIAL NOT NULL,
+	userName TEXT NOT NULL,
+	anonymous BOOLEAN NOT NULL default FALSE,
+	level NUMERIC NOT NULL default 0,
+	score NUMERIC NOT NULL default 0,
+	fullName TEXT NOT NULL
 );
--- TODO: Tabelle achievements verbessern
-CREATE TABLE main."achievements" (
-  "id" serial NOT NULL
-);
-CREATE TABLE main."collectors" (
-	"id" numeric NOT NULL ,
- 	"email" TEXT NOT NULL references main.users(email) ,
-  	"name" TEXT,
-  	PRIMARY KEY (id,email)	
-);
-CREATE TABLE main."nametable"(
-	"id" numeric PRIMARY KEY,
-	"name" TEXT NOT NULL unique
-	);
 
+--TABLE managing Collectors
 
+CREATE TABLE main.collector(
+	CID SERIAL unique PRIMARY KEY, 	 
+  	name TEXT NOT NULL
+);
+
+-- TABLE managing achievement
+-- one achievement is only identifible in combination with Collector ID
+
+CREATE TABLE main.achievements(
+	AID NUMERIC NOT NULL,
+	CID SERIAL NOT NULL references main.collector(CID),
+	description TEXT NOT NULL,
+	name TEXT NOT NULL,
+	value NUMERIC NOT NULL,
+	PRIMARY KEY (AID,CID)
+);
+
+-- TABLE managing the Achievements of specific User,
+ 
+CREATE TABLE main.UserAchievement(
+	AID NUMERIC,
+	CID SERIAL,
+	email TEXT references main.users(email),
+	progress numeric default 0,
+	last_updated TIMESTAMPTZ,
+	FOREIGN KEY (AID,CID) references main.achievements(AID,CID),
+	primary key (AID,CID,email)
+);
--- db/init/5-smallTest.sql
+++ db/init/5-smallTest.sql
@@ -1,15 +1,14 @@
 --create Users
-INSERT INTO main.users (email,name,anon,level,score) VALUES ('abc@gmx.de', 'abc', FALSE, 3, 300);
-INSERT INTO main.users (email,name,anon,level,score) VALUES ('abc1@gmx.de', 'abc', TRUE, 4, 450);
-INSERT INTO main.users (email,name,anon,level,score) VALUES ('abc2@gmx.de', 'abc2', TRUE, 3, 350);
-INSERT INTO main.users (email,name,anon,level,score) VALUES ('abc3@gmx.de', 'abc3', FALSE, 5, 500);
-INSERT INTO main.users (email,name,anon,level,score) VALUES ('abc4@gmx.de', 'abc4', FALSE, 6, 450);
-INSERT INTO main.users (email,name,anon,level,score) VALUES ('abc5@gmx.de', 'abc2', FALSE, 1, 650);
-
-
---create test connector
-DO $$ 
-begin
-	PERFORM main.Create_Collector('Test',0);
-	PERFORM main.Create_Collector('Test1',1);
-END $$;
+INSERT INTO main.users (email, userName, anonymous, level, score, fullName) VALUES ('abc@gmx.de', 'abc', FALSE, 3, 300, 'abc Mustermann');
+INSERT INTO main.users (email, userName, anonymous, level, score, fullName) VALUES ('abc1@gmx.de', 'abc', TRUE, 1, 500, 'abc1 Mustermann');
+INSERT INTO main.users (email, userName, anonymous, level, score, fullName) VALUES ('abc2@gmx.de', 'abc2', FALSE, -5, -120, 'abc2 Mustermann');
+INSERT INTO main.Collector (Name) VALUES ( 'GIT');
+INSERT INTO main.Collector (Name) VALUES ('JIRA');
+INSERT INTO main.achievements (AID,CID,description,name,value) VALUES (0,1,'#commits > 10', 'hard-workin', 100);
+INSERT INTO main.achievements (AID,CID,description,name,value) VALUES (1,1,'#commits > 100', 'harder-working', 500);
+INSERT INTO main.UserAchievement (AID,CID,email,progress,last_updated) VALUES (0,1,'abc@gmx.de',50,NOW());
+INSERT INTO main.UserAchievement (AID,CID,email,progress,last_updated) VALUES (1,1,'abc@gmx.de',50,NOW());
+INSERT INTO main.UserAchievement (AID,CID,email,progress,last_updated) VALUES (0,1,'abc1@gmx.de',50,NOW());
+INSERT INTO main.UserAchievement (AID,CID,email,progress,last_updated) VALUES (1,1,'abc1@gmx.de',50,NOW());
+INSERT INTO main.UserAchievement (AID,CID,email,progress,last_updated) VALUES (0,1,'abc2@gmx.de',50,NOW());
+INSERT INTO main.UserAchievement (AID,CID,email,progress,last_updated) VALUES (1,1,'abc2@gmx.de',50,NOW());
