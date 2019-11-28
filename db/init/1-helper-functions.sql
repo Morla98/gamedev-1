687d8ed60f94b9fb79d0b916a204f82bf666a304
leoncurth <leon.curth@gmx.de>
2019-11-28 21:19:51 +0100

    chore(db) add unlimited token size

--- db/init/1-helper-functions.sql
+++ db/init/1-helper-functions.sql
@@ -1,7 +1,15 @@
-CREATE OR REPLACE FUNCTION random_token (length int) 
+CREATE OR REPLACE FUNCTION random_token (length_rnd int) 
 returns TEXT 
 AS $$
+declare 
+counter numeric := 0;
+amount integer := length_rnd / 32;
+result_token TEXT default '';
 BEGIN
-	return (select substr(md5(random()::text), 0, length));
+	While (counter <= amount) LOOP
+	result_token := result_token || '' || (select md5(random()::text));
+	counter := counter + 1;
+	END LOOP;
+	return substr(result_token, 0, length_rnd + 1);
 END;
 $$LANGUAGE plpgsql;
