6ea3459e62df094a0552e4e34bd24890160b1e5c
Lukas Niehus <lniehus@live.de>
2019-12-01 14:42:32 +0100

    feat(controller aded together)

--- main/Backend/src/main/java/com/unihannover/gamedev/restcontroller/UserController.java
+++ main/Backend/src/main/java/com/unihannover/gamedev/restcontroller/UserController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.unihannover.gamedev.models.User;
+import java.util.List;
 
 @RestController
-@RequestMapping("/profile")
-public class CustomerController {
+public class UserController {
 
     @CrossOrigin(origins = "http://localhost:8082")
     @RequestMapping(value="/profile", method = RequestMethod.GET)
@@ -17,7 +17,28 @@ public class CustomerController {
         return returnUser;
     }
 
-    @CrossOrigin(origins = "http://localhost:8082")
+    @RequestMapping(value="/ranking", method = RequestMethod.GET)
+    public ArrayList<User> getUserList() {
+        ArrayList<User> users = new ArrayList<>();
+
+        User user1 = new User();
+        user1.firstName = "Jhon";
+        user1.lastName = "Doe";
+        user1.email = "Jhon@Doe.de";
+        user1.userName = "myUserName";
+        user1.level = 1;
+        user1.score = 0;
+        user1.anonymous = false;
+
+        if(!user1.anonymous) {
+            users.add(user1);
+        }
+
+        System.out.println(users.get(0).firstName);
+
+        return users;
+    }
+
     @RequestMapping(value="/profile", method = RequestMethod.POST)
     public void updateProfile(@RequestBody String name, String mail, boolean hidden) {
 
@@ -39,6 +60,4 @@ public class CustomerController {
         System.out.println(user1.toString());
 
     }
-
-
 }
