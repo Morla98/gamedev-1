40d96079ab367b5383b76186ab241eeeb97ca30e
Lukas Allermann <Lukas.Allermann@live.de>
2019-12-01 13:46:48 +0100

    feature(GeneralCollector): Collector can send POST Requests

--- GeneralCollector/main/src/main/java/com/unihannover/gamedev/component/AppStartupRunner.java
+++ GeneralCollector/main/src/main/java/com/unihannover/gamedev/component/AppStartupRunner.java
@@ -2,6 +2,13 @@ package com.unihannover.gamedev.component;
 
 import com.unihannover.gamedev.models.Metric;
 import com.unihannover.gamedev.repositories.MetricRepository;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -23,7 +30,7 @@ public class AppStartupRunner implements ApplicationRunner {
 
 
 
-    private String sendPostRequest() {
+    private String sendPostRequest2() {
         /*HttpURLConnection con = null;
         URL url;
         try {
@@ -71,16 +78,30 @@ public class AppStartupRunner implements ApplicationRunner {
         Runtime runtime = Runtime.getRuntime();
 
         try {
-            Process process = runtime.exec("curl -X POST -d '{\"collectorId\": 0,\"description\":\"Die Curl Posts funktionieren!\",\"name\":\"TEST\",\"value\":0}' -v -i 'http://devgame:8080/api/achievements'");
-            int resultCode = process.waitFor();
+            String url = "\"http://172.22.0.1:8082/api/achievements\"";
+            StringBuilder json = new StringBuilder();
+            json.append("\"{");
+            json.append("\\\"collectorId\\\": 0,  ");
+            json.append("\\\"description\\\":\\\"CollectorAchievement\\\",  ");
+            json.append("\\\"name\\\":\\\"CollectorAchievement\\\",  ");
+            json.append("\\\"value\\\": 0");
+            json.append("}\"");
 
+            String type = "\"Content-Type: application/json\"";
+            String type2 = "\"accept: */*\"";
+            String command = "curl -X POST " + url + " -H  " + type + " -H  " + type2 + " -d "+ json;
+            System.out.println("\n " + command + "\n");
+            Process process = runtime.exec(command);
+            int resultCode = process.waitFor();
+            System.out.println("\n " + command + "\n");
             if (resultCode == 0) {
                 System.out.println("ofisduhiuosdgfghfooijgd");
                 // all is good
             }
             else
             {
-                System.out.println(resultCode + "I am a dissapointment");
+                System.out.println("\n " + command + "\n");
+                System.out.println(resultCode + ": I am a dissapointment");
             }
         } catch (Throwable cause) {
             // process cause
@@ -88,6 +109,46 @@ public class AppStartupRunner implements ApplicationRunner {
         }
 
         return "";
+    }
+    private void sendPostRequest(){
+        String result = "";
+        String url = "http://172.22.0.1:8082/api/achievements";
+        HttpPost post = new HttpPost(url);
+
+        StringBuilder json = new StringBuilder();
+        json.append("{");
+        json.append("\"collectorId\": 0,");
+        json.append("\"description\": \"CollectorAchievement\",");
+        json.append("\"name\": \"CollectorAchievement\",");
+        json.append("\"value\": 0");
+        json.append("}");
+
+        try {
+            // send a JSON data
+            post.setHeader("Accept", "*/*");
+            post.setHeader("Content-type", "application/json");
+            post.setEntity(new StringEntity(json.toString()));
+
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpClient.execute(post);
+            result = EntityUtils.toString(response.getEntity());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("\n\nRETRY\n\n");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            sendPostRequest();
+        }
+        System.out.println("\n\n" + result + "\n\n");
     }
 
     @Bean
@@ -97,25 +158,10 @@ public class AppStartupRunner implements ApplicationRunner {
         };
     }
 
-    /*
-    private void test(){
-        String payload = "{\n" +
-                "  \"collectorId\": 0,\n" +
-                "  \"description\": \"string\",\n" +
-                "  \"name\": \"string\",\n" +
-                "  \"value\": 0\n" +
-                "}";
-        try {
-            sendPostRequest("");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    */
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // TODO: Init Achievements and send them to server
-        System.out.println("Test finished");
         sendPostRequest();
+        System.out.println("Test finished");
     }
 }
