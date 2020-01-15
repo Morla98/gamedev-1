1bcc78572b2e5fd62c8b7cf125879e8ff0338039
Tim Hollmann <hollmann@stud.uni-hannover.de>
2020-01-15 01:59:12 +0100

    feat(WebApp): Various frontent improvements

--- main/Webapp/src/app/ranking/ranking.component.ts
+++ main/Webapp/src/app/ranking/ranking.component.ts
@@ -11,33 +11,6 @@ import { User } from 'src/api/models';
 import { map, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 
-const ranks: Rank[] = [
-  {
-    name: 'Donald Quack',
-    points: 54,
-    monthly: 12,
-    daily: 2
-  },
-  {
-    name: 'Daisy Schlumpf',
-    points: 43,
-    monthly: 8,
-    daily: 1
-  },
-  {
-    name: 'Max MusterPerson',
-    points: 87,
-    monthly: 28,
-    daily: 6
-  },
-  {
-    name: 'Jhon Doe',
-    points: 60,
-    monthly: 40,
-    daily: 10
-  }
-];
-
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.component.html',
