e98756881963a0679ebdef6ce0a37a0d051c0ad0
FelixV <dd28924@googlemail.com>
2020-01-22 01:14:05 +0100

    fix achievement name

--- collectors/git/GitCollector/src/main/java/com/unihannover/gamedev/services/CollectorService.java
+++ collectors/git/GitCollector/src/main/java/com/unihannover/gamedev/services/CollectorService.java
@@ -206,7 +206,7 @@ public class CollectorService {
 		}
 		// only if collector was not known he should send the initalize Achievementlist and UserAchievementList
 		if(!reported){
-			httpService.sendList(aModelList, "http://devgame:8080/api/achievements");
+			CloseableHttpResponse res = httpService.sendList(aModelList, "http://devgame:8080/api/achievements");
 			if(uaModelList.size() > 0) {
 				httpService.sendList(uaModelList, "http://devgame:8080/api/user-achievements");
 			}
